992cc818ce08f4ac2be35f97a390144e38dd2d30
Jack Puschnigg <john.puschnigg@stvincent.edu>
2022-10-12 15:15:37 -0400

    Updated personal info

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ export default function About() {
     return (
         <div className="content">
             <h3>About</h3>
-            <p>Lifelong organizer. General reader. Pop culture fanatic. Internet aficionado. Thinker. Award-winning student. Prone to fits of apathy. Social media buff.</p>
+            <p>I am a senior at Saint Vincent College majoring in Computer Science and minoring in Mathematics. After graduation, I would love to work in full-stack development because I have a passion for front and back-end technologies and their design.</p>
         </div>
     )
 }
--- src/components/Interests.js
+++ src/components/Interests.js
@@ -2,8 +2,7 @@ export default function Interests() {
     return (
         <div className="content">
             <h3>Interests</h3>
-            <p>Outside of work, I enjoy jewelry-making and knitting. My favorite color is turquoise.
-                I enjoy watching the movie 'Inside Man' on repeat with my kids. I love to ice skate on the weekends!
+            <p>Outside of programming, I also enjoy learning new (human) languages, playing piano and violin, gardening, cooking, baking sourdough bread, and practicing meditation.
             </p>
         </div>
     )
